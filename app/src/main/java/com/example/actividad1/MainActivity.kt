865ae485071584e150121f08fd18152eb3e04845
eacriollo <andrescriollo_1990@hotmail.com>
2026-04-13 11:18:24 -0500

    pantalla principal

--- app/src/main/java/com/example/actividad1/MainActivity.kt
+++ app/src/main/java/com/example/actividad1/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.actividad1.ui.theme.Actividad1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Actividad1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Activiadad 1 aqui inicia la actividad",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                    PantallaInicial()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
+
 @Composable
-fun GreetingPreview() {
-    Actividad1Theme {
-        Greeting("Android")
+fun PantallaInicial (){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Actividad 1",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "Seleccionar Anime",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Ingresar a Mundo Pokemon")
+        }
+
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Ingresar a Mundo DragonBall")
+        }
+
     }
 }
